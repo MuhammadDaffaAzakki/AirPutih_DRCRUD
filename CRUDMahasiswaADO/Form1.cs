cda2889b26459d7741c4c1146f930a83f35656d8
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2026-04-09 15:46:10 +0700

    Membuat Variabel Koneksi dan Constructor

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -2,4 +2,17 @@
 using System.Data.SqlClient;
 using System.Windows.Forms;
 
+namespace CRUDMahasiswaADO
+{
+    public partial class FormMahasiswa : Form
+    {
+        private readonly SqlConnection conn;
+        private readonly string connectionString =
+            "Data Source=DaffaAzakki\\DAFFA_AZAKKI;Initial Catalog=DBAkademikADO;Integrated Security=True";
+        public FormMahasiswa()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(connectionString);
+        }
+
 
